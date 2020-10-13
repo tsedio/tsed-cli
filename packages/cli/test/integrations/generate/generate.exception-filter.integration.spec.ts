8e82360bb9dfe944d619c76109dfd3f7de09b4fc
david lutta <davidlutta2010@gmail.com>
2020-10-13 18:29:27 +0300

    Add Exception Filter template generation

---
+++ packages/cli/test/integrations/generate/generate.exception-filter.integration.spec.ts
@@ -0,0 +1,53 @@
+import {CliService, ProjectPackageJson} from "@tsed/cli-core";
+import {CliPlatformTest, FakeCliFs} from "@tsed/cli-testing";
+import {readFileSync} from "fs";
+import {resolve} from "path";
+import {GenerateCmd} from "../../../src";
+
+const TEMPLATE_DIR = resolve(__dirname, "..", "..", "..", "templates");
+
+function readFile(file: string) {
+  return readFileSync(`${__dirname}/${file}`, {encoding: "utf8"});
+}
+
+describe("Generate Exception Filter", () => {
+  beforeEach(() =>
+    CliPlatformTest.bootstrap({
+      templateDir: TEMPLATE_DIR,
+      commands: [GenerateCmd]
+    })
+  );
+  afterEach(() => CliPlatformTest.reset());
+
+  it("should generate a template with the right options", async () => {
+    const cliService = CliPlatformTest.get<CliService>(CliService);
+    const projectPackageJson = CliPlatformTest.get<ProjectPackageJson>(ProjectPackageJson);
+    // @ts-ignore
+    projectPackageJson.raw = {
+      name: "",
+      version: "1.0.0",
+      description: "",
+      scripts: {},
+      dependencies: {},
+      devDependencies: {}
+    };
+
+    await cliService.exec("generate", {
+      rootDir: "./project-data",
+      type: "exception-filter",
+      name: "Http"
+    });
+
+    expect(FakeCliFs.getKeys()).toEqual([
+      "project-name/src/filters",
+      "project-name/src/filters/HttpExceptionFilter.ts"
+    ]);
+
+    const result = FakeCliFs.entries.get("project-name/src/filters/HttpExceptionFilter.ts");
+
+    expect(result).toContain("import {Catch, PlatformContext, ExceptionFilterMethods} from \"@tsed/common\";");
+    expect(result).toContain("@Catch(Error)");
+    expect(result).toContain("export class HttpExceptionFilter implements ExceptionFilterMethods");
+    expect(result).toContain("catch(exception: Exception, ctx: PlatformContext) {");
+  });
+});
