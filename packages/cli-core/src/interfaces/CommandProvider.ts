e99c6942c6530494253bf4027f97ab91b1985513
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-04-05 13:04:28 +0200

    chore(cli-core): Add integration test

--- packages/cli-core/src/interfaces/CommandProvider.ts
+++ packages/cli-core/src/interfaces/CommandProvider.ts
@@ -3,35 +3,35 @@ import {Tasks} from "./Tasks";
 
 export type QuestionOptions<T = any> = Inquirer.QuestionCollection<T>;
 
-export interface CommandProvider {
+export interface CommandProvider<Ctx = any> {
   /**
    * Hook to create the main prompt for the command
    * See https://github.com/enquirer/enquirer for more detail on question configuration.
    * @param initialOptions
    */
-  $prompt?<T = any>(initialOptions: any): QuestionOptions<T> | Promise<QuestionOptions<T>>;
+  $prompt?<T = any>(initialOptions: Partial<Ctx>): QuestionOptions<T> | Promise<QuestionOptions<T>>;
 
   /**
    * Hook to map options
-   * @param options
+   * @param ctx
    */
-  $mapContext?(options: any): any;
+  $mapContext?(ctx: Partial<Ctx>): Ctx;
 
   /**
    * Run something before the exec hook
-   * @param options
+   * @param ctx
    */
-  $beforeExec?(options: any): Promise<any>;
+  $beforeExec?(ctx: Ctx): Promise<any>;
 
   /**
    * Run a command
-   * @param options
+   * @param ctx
    */
-  $exec(options: any): Tasks | Promise<Tasks>;
+  $exec(ctx: Ctx): Tasks | Promise<Tasks>;
 
   /**
    * Run commands after the npm/yarn install
-   * @param options
+   * @param ctx
    */
-  $postInstall?(options: any): Tasks | Promise<Tasks>;
+  $postInstall?(ctx: Ctx): Tasks | Promise<Tasks>;
 }
--- packages/cli-core/src/utils/mapCommanderOptions.ts
+++ packages/cli-core/src/utils/mapCommanderOptions.ts
@@ -3,7 +3,7 @@ import * as commander from "commander";
 export function mapCommanderOptions(commands: commander.Command[]) {
   const options: any = {};
   commands.forEach((command) => {
-    Object.entries(command)
+    Object.entries(command.opts())
       .filter(([key]) => !key.startsWith("_") && !["commands", "options", "parent", "rawArgs", "args"].includes(key))
       .forEach(([key, value]) => {
         options[key] = value;
