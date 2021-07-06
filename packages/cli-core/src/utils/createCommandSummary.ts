ec61adddd635dbe4003e31e7c9d81bb467532551
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-07-06 07:44:57 +0200

    feat(cli-core): Bump commander to v8.0.0

--- packages/cli-core/src/utils/createCommandSummary.ts
+++
@@ -1,7 +0,0 @@
-import {CommandArg} from "../interfaces/CommandParameters";
-
-export function createCommandSummary(name: string, args: {[arg: string]: CommandArg}) {
-  return Object.entries(args)
-    .reduce((cmd, [arg, {required}]) => [...cmd, required ? `<${arg}>` : `[${arg}]`], [name])
-    .join(" ");
-}
