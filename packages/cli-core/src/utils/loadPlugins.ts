99990234a2d3b920b34587da80177f7bf002cd0f
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-05-07 09:01:15 +0200

    fix(cli-core): emit warning instead of error when plugin loading fail

--- packages/cli-core/src/utils/loadPlugins.ts
+++ packages/cli-core/src/utils/loadPlugins.ts
@@ -1,6 +1,8 @@
 import {GlobalProviders, InjectorService} from "@tsed/di";
 import {CliFs} from "../services/CliFs";
 import {ProjectPackageJson} from "../services/ProjectPackageJson";
+import {figures} from "listr2";
+import chalk from "chalk";
 
 const all = (promises: any[]) => Promise.all(promises);
 
@@ -16,28 +18,33 @@ export async function loadPlugins(injector: InjectorService) {
   const promises = Object.keys(projectPackageJson.allDependencies)
     .filter((mod) => mod.startsWith(`@${name}/cli-plugin`) || mod.includes(`${name}-cli-plugin`))
     .map(async (mod) => {
-      const {default: plugin} = await fs.importModule(mod, rootDir);
+      try {
+        const {default: plugin} = await fs.importModule(mod, rootDir);
 
-      if (!injector.has(plugin)) {
-        const provider = GlobalProviders.get(plugin)?.clone();
+        if (!injector.has(plugin)) {
+          const provider = GlobalProviders.get(plugin)?.clone();
 
-        if (provider?.imports.length) {
-          await all(
-            provider.imports.map(async (token) => {
-              injector.add(token, GlobalProviders.get(token)?.clone());
+          if (provider?.imports.length) {
+            await all(
+              provider.imports.map(async (token) => {
+                injector.add(token, GlobalProviders.get(token)?.clone());
 
-              if (injector.settings.loaded) {
-                await injector.invoke(token);
-              }
-            })
-          );
+                if (injector.settings.loaded) {
+                  await injector.invoke(token);
+                }
+              })
+            );
+          }
+
+          injector.add(plugin, provider);
+
+          if (injector.settings.loaded) {
+            await injector.invoke(plugin);
+          }
         }
-
-        injector.add(plugin, provider);
-
-        if (injector.settings.loaded) {
-          await injector.invoke(plugin);
-        }
+        injector.logger.info(chalk.green(figures.tick), mod, "module loaded");
+      } catch (er) {
+        injector.logger.warn(chalk.red(figures.cross), "Fail to load plugin", mod);
       }
     });
 
