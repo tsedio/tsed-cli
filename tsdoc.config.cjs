f15c8f5f706fd2a71c2e122898c2678759c6c247
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-05-29 21:45:29 +0200

    chore: add commonjs package.json on docs

--- tsdoc.config.cjs
+++ tsdoc.config.cjs
@@ -1,13 +1,16 @@
+
 module.exports = {
   rootDir: process.cwd(),
   packagesDir: "packages/",
-  scanPatterns: ["<rootDir>/packages/**/lib/**/*.d.ts", "!node_modules"],
+  scanPatterns: [
+    "<rootDir>/packages/{cli,cli-core}/lib/types/**/*.d.ts",
+    "!**/*.spec.ts",
+    "!**/node_modules",
+    "!**/__mock__/**"
+  ],
   outputDir: "<rootDir>/docs/api",
   baseUrl: "/api",
   jsonOutputDir: "<rootDir>/docs/.vuepress/public",
   scope: "@tsed",
-  modules: {
-    "cli-core": "cli-core",
-    cli: "cli"
-  }
+  modules: {}
 };
