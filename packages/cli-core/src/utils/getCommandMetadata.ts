246645290497cc58a724491260bf363f8d81da31
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-21 21:56:24 +0200

    chore: bump dev dependencies

--- packages/cli-core/src/utils/getCommandMetadata.ts
+++ packages/cli-core/src/utils/getCommandMetadata.ts
@@ -3,9 +3,14 @@ import {CommandStoreKeys} from "../domains/CommandStoreKeys";
 import {CommandParameters} from "../interfaces/CommandParameters";
 
 export function getCommandMetadata(token: Type<any>) {
-  const {name, alias, args = {}, allowUnknownOption, description, options = {}} = Store.from(token)?.get(
-    CommandStoreKeys.COMMAND
-  ) as CommandParameters;
+  const {
+    name,
+    alias,
+    args = {},
+    allowUnknownOption,
+    description,
+    options = {}
+  } = Store.from(token)?.get(CommandStoreKeys.COMMAND) as CommandParameters;
 
   return {
     name,
