01a09c8cbf4b1695f00d304b5d2098bc9ccc9321
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-30 20:56:49 +0200

    feat(cli-core): add util to load yml or json file configuration

---
+++ packages/cli-core/src/services/CliLoadFile.spec.ts
@@ -0,0 +1,54 @@
+import {DITest} from "@tsed/di";
+import {CliLoadFile} from "@tsed/cli-core";
+import {catchAsyncError} from "@tsed/core";
+
+describe("CliLoadFile", () => {
+  beforeEach(() => DITest.create());
+  afterEach(() => DITest.reset());
+
+  it("should load yaml file", async () => {
+    const service = DITest.get<CliLoadFile>(CliLoadFile);
+
+    const result = await service.loadFile(__dirname + "/__mock__/settings.yml");
+
+    expect(result).toEqual({
+      architecture: "default",
+      convention: "default",
+      features: ["babel"],
+      platform: "express",
+      projectName: "project-example"
+    });
+  });
+
+  it("should not validate the schema", async () => {
+    const service = DITest.get<CliLoadFile>(CliLoadFile);
+
+    const error = await catchAsyncError(() =>
+      service.loadFile(__dirname + "/__mock__/settings.yml", {
+        type: "object",
+        additionalProperties: true,
+        properties: {
+          platform: {
+            type: "string",
+            enum: ["koa"]
+          }
+        }
+      })
+    );
+
+    expect(error?.message).toEqual(".platform must be equal to one of the allowed values. Allowed values: koa");
+  });
+
+  it("should load json file", async () => {
+    const service = DITest.get<CliLoadFile>(CliLoadFile);
+
+    const result = await service.loadFile(__dirname + "/__mock__/settings.json");
+    expect(result).toEqual({
+      architecture: "default",
+      convention: "default",
+      features: [],
+      platform: "express",
+      projectName: "test"
+    });
+  });
+});
