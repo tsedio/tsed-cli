c21626d438cc62e9012da4399815a2b644ae4321
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-02-23 18:08:32 +0100

    fix(cli-generate-swagger): fix CliYaml implementation

--- packages/cli-core/src/services/CliYaml.ts
+++ packages/cli-core/src/services/CliYaml.ts
@@ -1,34 +1,21 @@
 import {Inject, Injectable} from "@tsed/di";
 import JsYaml from "js-yaml";
-import {join} from "path";
 import {CliFs} from "./CliFs";
-import {ProjectPackageJson} from "./ProjectPackageJson";
 
 @Injectable()
 export class CliYaml {
   @Inject()
-  protected projectPackageJson: ProjectPackageJson;
-
-  @Inject()
   protected fs: CliFs;
 
   async read(path: string) {
-    const file = !this.fs.exists(path) ? this.fs.findUpFile(this.projectPackageJson.dir, path) : path;
+    const content = await this.fs.readFile(path, {encoding: "utf8"});
 
-    if (file) {
-      const content = await this.fs.readFile(file, {encoding: "utf8"});
-
-      return JsYaml.load(content);
-    }
-
-    return {};
+    return JsYaml.load(content);
   }
 
   async write(path: string, obj: any) {
     const content = JsYaml.dump(obj);
 
-    const file = this.fs.findUpFile(this.projectPackageJson.dir, path) || join(this.projectPackageJson.dir, path);
-
-    return this.fs.writeFile(file, content, {encoding: "utf8"});
+    return this.fs.writeFile(path, content, {encoding: "utf8"});
   }
 }
