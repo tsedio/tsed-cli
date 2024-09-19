5f572d0f60391285f425ad6dc2fc7c103e001032
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-09-19 17:08:26 +0200

    fix(cli): fix plugin module resolution

--- packages/cli/src/bin/tsed.ts
+++ packages/cli/src/bin/tsed.ts
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 import {register} from "node:module";
-import {pathToFileURL} from "node:url";
+import {fileURLToPath, pathToFileURL} from "node:url";
 
-register(pathToFileURL(`${import.meta.dirname}/../alias.hook.js`));
+register(pathToFileURL(`${import.meta.dirname}/../loaders/alias.hook.js`), {
+  parentURL: import.meta.dirname,
+  data: {
+    "@tsed/core": fileURLToPath(import.meta.resolve("@tsed/core")),
+    "@tsed/di": fileURLToPath(import.meta.resolve("@tsed/di")),
+    "@tsed/schema": fileURLToPath(import.meta.resolve("@tsed/schema")),
+    "@tsed/cli-core": fileURLToPath(import.meta.resolve("@tsed/cli-core")),
+    "@tsed/cli": fileURLToPath(import.meta.resolve("@tsed/cli"))
+  },
+  transferList: []
+});
 
 const {Cli} = await import("../Cli.js");
 
