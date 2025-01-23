65ed90f4a62d0316a165b8143409278ef989675c
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-01-23 18:10:36 +0100

    fix(cli): fix alias module resolution on Windows OS

--- packages/cli/src/bin/tsed.ts
+++ packages/cli/src/bin/tsed.ts
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 import {register} from "node:module";
+import {join} from "node:path";
 import {fileURLToPath, pathToFileURL} from "node:url";
 
 const EXT = process.env.CLI_MODE === "ts" ? "ts" : "js";
 
-register(pathToFileURL(`${import.meta.dirname}/../loaders/alias.hook.${EXT}`), {
+register(pathToFileURL(join(import.meta.dirname, `../loaders/alias.hook.${EXT}`)), {
   parentURL: import.meta.dirname,
   data: {
-    "@tsed/core": fileURLToPath(import.meta.resolve("@tsed/core")),
-    "@tsed/di": fileURLToPath(import.meta.resolve("@tsed/di")),
-    "@tsed/schema": fileURLToPath(import.meta.resolve("@tsed/schema")),
-    "@tsed/cli-core": fileURLToPath(import.meta.resolve("@tsed/cli-core")),
-    "@tsed/cli": fileURLToPath(import.meta.resolve("@tsed/cli"))
+    "@tsed/core": import.meta.resolve("@tsed/core"),
+    "@tsed/di": import.meta.resolve("@tsed/di"),
+    "@tsed/schema": import.meta.resolve("@tsed/schema"),
+    "@tsed/cli-core": import.meta.resolve("@tsed/cli-core"),
+    "@tsed/cli": import.meta.resolve("@tsed/cli")
   },
   transferList: []
 });
--- packages/cli/src/loaders/alias.hook.ts
+++ packages/cli/src/loaders/alias.hook.ts
@@ -1,9 +1,16 @@
-import generateAliasesResolver from "esm-module-alias";
+function generateAliasesResolver(aliases: Record<string, string>, options?: any) {
+  return (specifier: any, parentModuleURL: any, defaultResolve: any) => {
+    if (aliases[specifier]) {
+      return defaultResolve(aliases[specifier], parentModuleURL);
+    }
+
+    return defaultResolve(specifier, parentModuleURL);
+  };
+}
 
 let resolver: any = null;
 
 export async function initialize(aliases: Record<string, string>) {
-  // Receives data from `register`.
   resolver = generateAliasesResolver(aliases);
 }
 
