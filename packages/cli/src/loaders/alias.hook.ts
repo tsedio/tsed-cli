4436d786325704f9744b32a7cd4a4e1c925dc09a
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-01-23 18:38:13 +0100

    fix(cli): try to resolve correctly the file path on Windows

--- packages/cli/src/loaders/alias.hook.ts
+++ packages/cli/src/loaders/alias.hook.ts
@@ -1,10 +1,18 @@
-import generateAliasesResolver from "esm-module-alias";
+function generateAliasesResolver(aliases: Record<string, string>, options?: any) {
+  return (specifier: any, parentModuleURL: any, defaultResolve: any) => {
+    if (aliases[specifier]) {
+      return defaultResolve(aliases[specifier], parentModuleURL);
+    }
+
+    return defaultResolve(specifier, parentModuleURL);
+  };
+}
 
 let resolver: any = null;
 
 export async function initialize(aliases: Record<string, string>) {
   // Receives data from `register`.
-  console.debug("Initializing alias hook with aliases", aliases);
+  // console.debug("Initializing alias hook with aliases", aliases);
 
   resolver = generateAliasesResolver(aliases);
 }
