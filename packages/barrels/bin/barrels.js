35079becfa35c19b0c7b7a97680db4b8c03830b0
YG Park <ygpark80@gmail.com>
2025-03-06 21:40:40 +0900

    yarn lint:fix

--- packages/barrels/bin/barrels.js
+++ packages/barrels/bin/barrels.js
@@ -3,7 +3,11 @@ import {generateBarrels} from "./generate-barrel.js";
 import {getConfig} from "./get-config.js";
 
 async function build() {
-  const {directory = ["./src"], exclude = ["**/__mock__", "**/__mocks__", "**/*.spec.ts", "**/*.benchmark.ts"], noSemicolon = false} = await getConfig();
+  const {
+    directory = ["./src"],
+    exclude = ["**/__mock__", "**/__mocks__", "**/*.spec.ts", "**/*.benchmark.ts"],
+    noSemicolon = false
+  } = await getConfig();
 
   await generateBarrels({exclude, directory, cwd: process.cwd(), noSemicolon});
 }
