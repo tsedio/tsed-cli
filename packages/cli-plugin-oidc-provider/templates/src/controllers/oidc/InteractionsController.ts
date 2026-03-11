c55dde15e9f006a337712feb9b22b3d5800d1d13
Camille Foubert <camille.foubert.ext@clubmed.com>
2026-03-11 10:27:43 +0100

    fix: children interactions must be ordered to maintain correct behavior

--- packages/cli-plugin-oidc-provider/templates/src/controllers/oidc/InteractionsController.ts
+++ packages/cli-plugin-oidc-provider/templates/src/controllers/oidc/InteractionsController.ts
@@ -1,12 +1,16 @@
 import {Interactions, OidcCtx} from "@tsed/oidc-provider";
 import {Get, Name} from "@tsed/schema";
 import {PathParams} from "@tsed/platform-params";
-import * as interactions from "../../interactions/index.js";
+import {LoginInteraction, ConsentInteraction, CustomInteraction} from "../../interactions/index.js";
 
 @Name("Oidc")
 @Interactions({
   path: "/interaction/:uid",
-  children: Object.values(interactions)
+  children: [
+    LoginInteraction,
+    ConsentInteraction,
+    CustomInteraction,
+  ]
 })
 export class InteractionsController {
   @Get("/:name?")
